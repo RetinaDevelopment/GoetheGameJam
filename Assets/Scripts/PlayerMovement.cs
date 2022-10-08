d445527bf6140b39f65d928a2f8845209f367eca
Stefan0134 <stefanburlacu07@gmail.com>
2022-10-08 19:54:23 +0300

    sasasas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,21 +9,29 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 25f;
     private bool isFacingRight = true;
-
+    public Animator animator;
+    
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     void Update()
     {
+
         horizontal = Input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump") && IsGrounded()) rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        animator.SetFloat("walk" , Math.Abs(horizontal));
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            animator.SetBool("jump", true);
+        }
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         Flip();
     }
 
     private void FixedUpdate()
     {
+        animator.SetBool("jump", !IsGrounded());
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
