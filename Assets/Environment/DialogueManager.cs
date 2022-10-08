ee9c0fe798d11f92bb4ff6bc5964406f269a7ce2
Stefan0134 <stefanburlacu07@gmail.com>
2022-10-09 00:49:19 +0300

    animatii si ce am incercat la dialog(inomplet)

--- Assets/Environment/DialogueManager.cs
+++ Assets/Environment/DialogueManager.cs
@@ -5,9 +5,46 @@ using UnityEngine;
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> sentences;
+    public static DialogueManager instance;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         sentences = new Queue<string>();
+    }
+
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("starting convo with");
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+
+    void EndDialogue()
+    {
+        Debug.Log("End Conversation");
+        anim.SetBool("Pula", false);    
     }
 }
