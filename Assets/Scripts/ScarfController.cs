79af5f963f0abbf838db3f77be653717346d758e
Sorin <sorin.andrei.tudose@gmail.com>
2022-10-08 23:56:25 +0300

    Final environment changes

--- Assets/Scripts/ScarfController.cs
+++ Assets/Scripts/ScarfController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ScarfController : MonoBehaviour
 {
+    void start()
+    {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
