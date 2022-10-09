4af88e260b4b6c5c68c8608420574a3642bff4e1
Loloowastaken <mihnea_dobre@yahoo.com>
2022-10-09 11:34:27 +0300

    YIPPEPEPEPPE (pls work) (no conflicts pls) (thanks)
    :-1:

--- Assets/code.cs
+++ Assets/code.cs
@@ -10,14 +10,12 @@ public class code : MonoBehaviour
     public TMP_InputField CodeInput;
     public string SceneName1;
     public string SceneName2; 
-    public string condition1;
-    public string condition2;
     public void loadscene()
     {
-        if (CodeInput.text==condition1)
-        SceneManager.LoadScene(SceneName1, LoadSceneMode.Single);
-        if (CodeInput.text==condition2)
-            SceneManager.LoadScene(SceneName2, LoadSceneMode.Single);
+        if (CodeInput.text=="1212")
+        SceneManager.LoadScene("secret1", LoadSceneMode.Single);
+        if (CodeInput.text=="7244")
+            SceneManager.LoadScene("secret2", LoadSceneMode.Single);
 
     }
 }
